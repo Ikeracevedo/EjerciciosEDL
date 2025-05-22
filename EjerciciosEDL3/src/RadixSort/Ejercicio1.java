5867b01f2267ff43169d47516a65c080700fe9fc
Iker Acevedo <acevedo_0413@hotmail.com>
2025-05-22 18:29:26 -0500

    Mejora Lab1 Lisra de colas - Iker

--- EjerciciosEDL3/src/RadixSort/Ejercicio1.java
+++ EjerciciosEDL3/src/RadixSort/Ejercicio1.java
@@ -1,7 +1,9 @@
 package RadixSort;
-
+import java.util.Queue;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayDeque;
+
 public class Ejercicio1 {
 
     //Metodo estatico que retorna un numero entero
@@ -40,15 +42,15 @@ public class Ejercicio1 {
         //Por medio del bucle for se va sacando letra por letra, del string hexadecimal
         //Hasta llegar a 0
         for (int pos = tamaño -1 ; pos >= 0; pos--) {
-            //Crea una lista de listas String donde cada espacio de la lista
+            //Crea una lista de colas String donde cada espacio de la lista
             //Es para un caracter, en total 16 espacios
             // 0 - 1 - 2 - 3 - 4 - 5 - 6 - 7 - 8 - 9 - A - B - C - D - E - F
             //Luego dentro de cada espacio hay otra lista que recolecta todos los numeors para cada cubeta
-            List<List<String>> cubetas = new ArrayList<>();
+            List<Queue<String>> cubetas = new ArrayList<>();
             //Crea los espacios en memoria hasta 16 espacios del hexadecimal
             for (int i = 0; i < 16; i++) {
                 //Crea los espacios en memoria
-                cubetas.add(new ArrayList<>());
+                cubetas.add(new ArrayDeque<String>());
             }
 
             //Se itera sobre el arreglo con la variable como num  
@@ -66,7 +68,6 @@ public class Ejercicio1 {
                     //En caso de que el numero sea mas corto de la llave normal 
                     digit = 0;
                 }
-
                 //Se toma cada posicion de los 16 espacios y en cada espacio lo llena con el numero 
                 cubetas.get(digit).add(num);
             }
@@ -74,13 +75,13 @@ public class Ejercicio1 {
             int puntero = 0;
 
             //Por medio de un bucle for y por el iterable de cubo en cubetas recorre cada uno de los 16 espacios
-            for (List<String> cubo : cubetas) {
+            for (Queue<String> cubo : cubetas) {
                 //Por cada cubo se itera en el con la variable val 
-                for (String val : cubo) {
-                    //Se asigna cada número val en orden a arr[puntero]
+                while (!cubo.isEmpty()) {
+                   //Se asigna cada número val en orden a arr[puntero]
                     //Incrementamos puntero para llenar el arreglo arr en orden
-                    arr[puntero++] = val;
-                }   
+                    arr[puntero++] = cubo.poll(); 
+                }
             }
         }
     }
